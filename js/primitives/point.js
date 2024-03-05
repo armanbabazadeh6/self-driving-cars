1b48ad360bcf561a28188d74a0b46caae28ee319
Arman Babazadeh <armanbabazadeh61@gmail.com>
2024-03-05 02:51:58 -0500

    MouseEditor Added

--- js/primitives/point.js
+++ js/primitives/point.js
@@ -8,11 +8,18 @@ class Point {
         return this.x == point.x && this.y == point.y
     }
 
-    draw(ctx, size = 18, color = "black") {
+    draw(ctx, { size = 18, color = "black", outline = false } = {}) { // Group properties as object so we can pass it in any order we want
         const rad = size / 2; //draw points as circles
         ctx.beginPath();
         ctx.fillStyle = color;
         ctx.arc(this.x, this.y, rad, 0, Math.PI * 2); //Draw this at this x and y full circle starting at 0 degree's and ending at 360 degree's
         ctx.fill();
+        if (outline) {
+            ctx.beginPath();
+            ctx.lineWidth = 2;
+            ctx.strokeStyle = "red";
+            ctx.arc(this.x, this.y, rad * 0.6, 0, Math.PI * 2);
+            ctx.stroke();
+        }
     }
 }
